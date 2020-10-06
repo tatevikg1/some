3a79274f7aa979113b5b719f53f6cad881f287c1
Ta Gr <tatevikg1@gmail.com>
2020-10-06 21:23:10 +0400

    heroku 1

--- config/database.php
+++ config/database.php
@@ -137,7 +137,7 @@ return [
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
+            'password' => en(SQL: select count(*) as aggregate from "users" where "email" = v('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
