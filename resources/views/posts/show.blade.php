9774c4a8da034c6a94d576bc1560cd45bd7d280a
ta <tatevikg1@gmail.com>
2021-04-20 18:17:04 +0400

    post.index for not loged in users

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,18 +10,20 @@
         </div>
         <div class="col-4 white_bckgr">
             <div class="d-flex align-items-center  mt-3 d-flex">
-                <div class="row align-items-center  pl-3 " onclick="location.href=`{{ route('profile.show',  Auth::user()->id) }} `">
+                <div class="row align-items-center  pl-3 " @auth onclick="location.href=`{{ route('profile.show',  Auth::user()->id) }} `" @endauth>
                     <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle" style="max-width:40px">
                     <div class="m-3 font-weight-bold"> {{ ucfirst($post->user->username) }}</div>
                 </div>
                 <div style="position:absolute; right:0;">
                     <div class="font-weight-bold">
                         <div class="">
+                        @auth
                             @if ($post->user->id == Auth::user()->id)
                                 <div class="mr-1">
                                     <a href="{{ route('post.destroy', $post->id) }}">Delete</a>
                                 </div>
                             @endif
+                        @endauth
                         </div>
                     </div>
                 </div>
