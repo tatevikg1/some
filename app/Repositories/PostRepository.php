5926c2f951bd861eae66395b19cb3867957117e6
Tatevik <tatevikg1@gmail.com>
2023-05-27 19:58:02 +0400

    10-docker: displaying image form docker container

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -26,8 +26,14 @@ class PostRepository
     {
         $data = $request->validated();
         $imagePath = request('image')->store('uploads', 'public');
-        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
-        $image->save();
+        try {
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+            $image->save();
+        } catch (\Throwable $throwable)
+        {
+
+        }
+
 
         $request->user()->posts()->create([
             'caption' => $data['caption'],
--- start.sh
+++ start.sh
@@ -14,20 +14,21 @@ fi
 #php artisan migrate
 #php artisan db:seed
 
-## setup file permissions
-#sudo usermod -a -G www-data t
-#sudo chown -R $USER:www-data .
-#sudo find . -type f -exec chmod 664 {} \;
-#sudo find . -type d -exec chmod 775 {} \;
-#sudo chgrp -R www-data storage bootstrap/cache
-#sudo chmod -R ug+rwx storage bootstrap/cache
+# setup file permissions
+sudo usermod -a -G www-data t
+sudo chown -R $USER:www-data .
+sudo find . -type f -exec chmod 664 {} \;
+sudo find . -type d -exec chmod 775 {} \;
+sudo chgrp -R www-data storage bootstrap/cache
+sudo chmod -R ug+rwx storage bootstrap/cache
 
 # make this file executable
 sudo chmod +x start.sh
 
-nvm use 14
-npm run dev
+#nvm use 14
+#npm run dev
 
 # php artisan passport:client --personal
+php artisan storage:link
 
 exit
