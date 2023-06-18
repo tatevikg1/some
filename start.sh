c76610539d0b768ec4b558986955d73d43d574b0
Tatevik <tatevikg1@gmail.com>
2023-06-18 21:32:51 +0400

    11: redis in docker-compose.yml

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ fi
 sudo usermod -a -G www-data t
 sudo chown -R $USER:www-data .
 sudo find . -type f -exec chmod 664 {} \;
-sudo find . -type d -exec chmod 775 {} \;
+sudo find . -type d -exec chmod 755 {} \;
 sudo chgrp -R www-data storage bootstrap/cache
 sudo chmod -R ug+rwx storage bootstrap/cache
 
